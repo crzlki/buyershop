6a8e53b89003298809f6df5b4f5354af3eca6491
池瑞镇 <12508160+crzlki@user.noreply.gitee.com>
2023-03-02 10:50:22 +0800

    prod config

--- config.js
+++ config.js
@@ -11,9 +11,15 @@ var BASE = {
     manager: "http://admin-api.nbuxinxiren.cn:8887"
   },
   API_PROD: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    // common: "https://common-api.pickmall.cn",
+    // buyer: "https://buyer-api.pickmall.cn",
+    // seller: "https://store-api.pickmall.cn",
+    // manager: "https://admin-api.pickmall.cn"
+    common: "http://common-api.nbuxinxiren.cn:8890",
+    // common: "http://34.85.84.1:8890",
+    // buyer: "http://34.85.84.1:8888",
+    buyer: "http://buyer-api.nbuxinxiren.cn:8888",
+    seller: "http://store-api.nbuxinxiren.cn:8889",
+    manager: "http://admin-api.nbuxinxiren.cn:8887"
   }
 };
